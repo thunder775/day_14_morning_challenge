1fefd79f1cc85c17518ff0cb31af74df0796934e
McLarenCollege <hello@mclarencollege.in>
2019-10-18 08:13:40 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,29 @@
+// Challenge 1
+// Write test cases for the next two challenges and let them fail
+
+// Challenge 2
+// Create a function that takes a number and returns its multiplicative persistence,
+// which is the number of times you must multiply the digits in num until you reach a single digit.
+// multiplicative(39) ➞ 3
+/// Because 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4 and 4 has only one digit.
+int multiplicative(int number) {
+  return null;
+}
+
+// Challenge 3
+// Mexican Wave Simulator
+/// The wave (known as a Mexican wave in the English-speaking world outside North
+/// America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms.
+//  https://s3.amazonaws.com/edabit-images/mex.gif
+//  Create a function that takes a string and turns it into a Mexican Wave.
+//  Examples
+//  wave("edabit") ➞ ["Edabit", "eDabit", "edAbit", "edaBit", "edabIt", "edabiT"]
+
+//  wave("just do it") ➞ ["Just do it", "jUst do it", "juSt do it", "jusT do it", "just Do it", "just dO it", "just do It", "just do iT"]
+
+//  wave(" ") ➞ []
+List<String> wave(String word) {
+  return null;
+}
+
+main() {}
---
+++ test/Day_14_Challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:Day_14_Challenge/Day_14_Challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
